a5db5f70ea6869a189d441cf339c1ef9aad2e305
whitehox <oketegah@gmail.com>
2019-10-14 14:37:11 +0100

    completed base form submit functionality

--- config/db_conncetion.php
+++ config/db_conncetion.php
@@ -7,7 +7,6 @@ function openConnection() {
   $dbname = 'dev-app-week';
   $conn;
   try {
-
     $conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpassword, array(PDO::ATTR_PERSISTENT => true));
     return $conn;
   } catch (PDOException $e) {
