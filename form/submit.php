84debd00efc93da86e8f87ebdc0d985ec8226e30
whitehox <oketegah@gmail.com>
2019-10-14 12:48:33 +0100

    fixed form merge conflict

---
+++ form/submit.php
@@ -0,0 +1,14 @@
+<?php
+require('../config/db_connection.php')
+
+if(isset($_POST['submit'])) {
+  $name = strip_tags($_POST['']);
+  $office = strip_tags($_POST['']);
+  $office_address = strip_tags($_POST['']);
+  $team = strip_tags($_POST['']);
+  $email = strip_tags($_POST['']);
+  $phone_number = strip_tags($_POST['']);
+
+}
+
+ ?>
