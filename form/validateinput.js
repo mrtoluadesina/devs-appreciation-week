9da011484309703dd792ad0ac96dd6f37916e7ee
whitehox <oketegah@gmail.com>
2019-10-15 10:53:45 +0100

    completed form validation

--- form/validateinput.js
+++ form/validateinput.js
@@ -6,6 +6,8 @@ $(document).ready(function() {
     const team = $('#team').val();
     const email = $('#email').val();
     const companywebsite = $('#companywebsite').val();
+    const pattern = /@gmail|@yahoo/g;
+    const urlPattern = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/g;
 
     if (
       !username ||
@@ -21,8 +23,26 @@ $(document).ready(function() {
       $('.errorMessage').css('border-radius', '4px');
       $('.errorMessage').css('font-size', '1rem');
       $('.errorMessage').css('margin', '10px auto');
-
       $('.errorMessage').html('All Fields are required');
+    } else {
+      if (email.match(pattern)) {
+        e.preventDefault();
+        $('.errorMessage').css('background', '#f8d7da');
+        $('.errorMessage').css('color', '#721c24');
+        $('.errorMessage').css('border-radius', '4px');
+        $('.errorMessage').css('font-size', '1rem');
+        $('.errorMessage').css('margin', '10px auto');
+        $('.errorMessage').html('Please enter your work email address');
+      }
+      if (!companywebsite.match(urlPattern)) {
+        e.preventDefault();
+        $('.errorMessage').css('background', '#f8d7da');
+        $('.errorMessage').css('color', '#721c24');
+        $('.errorMessage').css('border-radius', '4px');
+        $('.errorMessage').css('font-size', '1rem');
+        $('.errorMessage').css('margin', '10px auto');
+        $('.errorMessage').html('Please enter a valid url');
+      }
     }
   });
 });
